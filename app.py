5c8f4dabb449c47f459bf7bb0d1a75f87dc2a7fa
Venkatadattak <60424923+Venkatadattak@users.noreply.github.com>
2020-08-03 18:34:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+import jsonify
+import requests
+import pickle
+import numpy as np
+import sklearn
+from sklearn.preprocessing import StandardScaler
+app = Flask(__name__)
+model = pickle.load(open('classifier.pickle', 'rb'))
+@app.route('/',methods=['GET'])
+def Home():
+    return render_template('index.html')
+
+
+standard_to = StandardScaler()
+@app.route("/predict", methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        SepalLengthCm = float(request.form['SepalLengthCm'])
+        SepalWidthCm = float(request.form['SepalWidthCm'])
+
+        PetalLengthCm = float(request.form['PetalLengthCm'])
+
+        PetalWidthCm = float(request.form['PetalWidthCm'])
+
+        
+        prediction=model.predict([[SepalLengthCm,SepalWidthCm,PetalLengthCm,PetalWidthCm]])
+        output=prediction[0]
+        
+        return render_template('index.html',prediction_text="The flower is {}".format(output))
+    else:
+        return render_template('index.html')
+    
+
+if __name__=="__main__":
+    app.run(debug=True)
+
---
+++ readme.txt
@@ -0,0 +1,12 @@
+open Anaconda prompt
+
+pip install flask
+pip install requirements
+
+Enter python app.py
+		if it asks any thing to install then install it.
+		pip install _______
+python app.py
+192.168.0.1/ kind of thing will be visible copy and paste in browser.
+
+ _______END___________
